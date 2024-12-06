077605fcddeee32796087fb0e86ef77fda384f38
medkh6 <22084@supnum.mr>
2024-12-06 01:07:31 +0100

    suprimer tous les fichiers

--- src/app/app.state.ts
+++
@@ -1,6 +0,0 @@
-import { CartState } from './state/cart/cart.reducer';
-import { ProductsState } from './state/products/products.state';
-
-export interface AppState {
-  cart: CartState;
-}
--- src/app/state/cart/cart.state.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from "@ngrx/store";
-
-export const addToCart = createAction("[Item] add to cart");
-export const removeFromCart = createAction("[Item] Remove from cart");
